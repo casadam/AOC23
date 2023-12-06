0618aa15c6af039a5d03b616b9331fb5d7ca0389
casadam <144391338+casadam@users.noreply.github.com>
2023-12-06 17:11:59 +0100

    Add files via upload

---
+++ day4.h
@@ -0,0 +1,18 @@
+#ifndef AOC_DAY4_H
+#define AOC_DAY4_H
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct Scratch {
+    int ID;
+    int uses;
+    int matches;
+} Scratch;
+
+void SCRATCH();
+
+#endif
